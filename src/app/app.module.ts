f0e3bcb5dc9e8fac076fb4c65bc4d3766debc02c
MohamedAmineDerouiche <mohderouiche250881@gmail.com>
2020-11-05 13:35:02 +0100

    component information created/button added/google map whith api key aded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from'@angular/common/http'
+import {AgmCoreModule} from '@agm/core'
+import { ComponentInformationComponent } from './component-information/component-information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentInformationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey:"AIzaSyD-vPPQ4Y-W_BkFVxdkD1IOg2db-vdx-rM"
+    })
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
