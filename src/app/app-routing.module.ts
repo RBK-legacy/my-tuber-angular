c994d65d819fa67d192efd8f29f0f1b23f6a952c
Dhia-Fattoum <dhiafattoum@outlook.com>
2020-11-05 17:44:21 +0100

    fixing errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [];
+import {HomeComponent} from "./home/home.component"
+const routes: Routes = [
+  {path: 'home', component:HomeComponent}
+  // {path: 'find', component:findDriver}
+  // {path: 'aboutUs', component:aboutUs}
+  // {path: 'joinUs', component:joinUs}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
