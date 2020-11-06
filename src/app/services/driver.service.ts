59aabd75a4fc9d372418244cdcd9a12128181dd4
sami affes <sami_affes@yahoo.com>
2020-11-06 02:07:38 +0100

    component signup works good and send the data to the database

---
+++ src/app/services/driver.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:5000/drivers';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DriverService {
+
+  constructor(private http: HttpClient) { }
+
+  createLogin(data): Observable<any> {
+    return this.http.post(baseUrl + '/signin', data);
+  }
+
+  createRegister(data): Observable<any> {
+    return this.http.post(baseUrl + '/signup', data);
+  }
+}
