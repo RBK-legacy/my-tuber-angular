9df819aa40ff8fcb2d9a31757ccdc2fba295c6eb
Kais Temimi <kassus007@gmail.com>
2020-11-08 12:49:55 -0800

    some styling css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-
-import {HomeComponent} from "./home/home.component"
-// const routes: Routes = [
-//   {path: 'home', component:HomeComponent},
-//   {path: 'user', component:UserComponent}
-//   // {path: 'find', component:findDriver}
-//   // {path: 'aboutUs', component:aboutUs}
-//   // {path: 'joinUs', component:joinUs}
-// ];
-
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { UserComponent } from './user/user.component';
 
 
 
+import {HomeComponent} from "./home/home.component"
+const routes: Routes = [
+//   {path: 'home', component:HomeComponent},
+//   {path: 'user', component:UserComponent}
+//   // {path: 'find', component:findDriver}
+//   // {path: 'aboutUs', component:aboutUs}
+//   // {path: 'joinUs', component:joinUs}
+];
 
 
-const routes: Routes = [];
+
+
+
+
 
 
 
